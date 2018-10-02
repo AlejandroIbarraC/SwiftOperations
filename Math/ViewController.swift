386da1b645a45625bc0eef4ccd4dc046ec98f524
Alejandro I <josealejandroibarra@hotmail.com>
2018-10-02 10:55:17 -0600

    initial commit

--- Math/ViewController.swift
+++ Math/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
+extension Float {
+    var cleanValue: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBOutlet weak var box1: UITextField!
+    
+    @IBOutlet weak var box2: UITextField!
+    
+    @IBOutlet weak var suma: UILabel!
+    
+    @IBOutlet weak var resta: UILabel!
+    
+    @IBOutlet weak var multiplicacion: UILabel!
+    
+    @IBOutlet weak var division: UILabel!
+    
+    // Calcula suma, resta, multiplicacion y division y los muestra en interfaz.
+    @IBAction func calcular(_ sender: Any) {
+        let num1:Float = Float(box1.text!)!
+        let num2:Float = Float(box2.text!)!
+        
+        let mas = (num1 + num2).cleanValue
+        let menos = (num1 - num2).cleanValue
+        let por = (num1 * num2).cleanValue
+        let dividido = (num1 / num2).cleanValue
+        
+        self.suma.text = "Suma: " + String(mas)
+        self.resta.text = "Resta: " + String(menos)
+        self.multiplicacion.text = "Multiplicación: " + String(por)
+        self.division.text = "División: " + String(dividido)
+        
+    }
 
 }
 
